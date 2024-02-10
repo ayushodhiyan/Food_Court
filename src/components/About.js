83a90674f578fb30a3b6ec8bbd199f0e282f68f6
ayushodhiyan <ayushsaini5911@gmail.com>
2024-02-10 13:44:35 +0530

    roting added to the project

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <hi>ABOUT US</hi>
+    </div>
+  )
+}
+
+export default About
--- src/index.js
+++ src/index.js
@@ -3,15 +3,32 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter,RouterProvider } from 'react-router-dom';
+import About from './components/About';
+import Error from './components/Error';
+import Contact from './components/Contact';
+
+const routerProvider=createBrowserRouter([
+  {
+    path:"/",
+    element:<App/>,
+    errorElement:<Error/>,
+  },
+  {
+    path:"/about",
+    element:<About/>,
+  },
+  {
+    path:"/contact",
+    element:<Contact/>,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={routerProvider} />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
