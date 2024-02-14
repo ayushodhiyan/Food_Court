43623993759767189db2a0a0802639292a35cb1e
ayushodhiyan <ayushsaini5911@gmail.com>
2024-02-14 14:43:37 +0530

    added some data into about us page

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,44 @@
 import React from 'react'
 
-const About = () => {
-  return (
-    <div>
-      <h1>ABOUT US</h1>
-    </div>
-  )
+class About extends React.Component{
+  constructor(props){
+    super(props);
+
+    this.state={
+      userInfo:{
+        name:"user-name",
+        location:"user-location",
+        bio:"user-bio",
+        email:"user-email",
+      }
+    };
+  }
+  async componentDidMount(){
+
+    const data=await fetch("https://api.github.com/users/ayushodhiyan");
+    const json=await data.json();
+    this.setState({
+      userInfo:json,
+    })
+  }
+  
+  
+  
+  
+  render(){
+    const{name,location,bio,avatar_url,email}=this.state.userInfo;
+    return(
+      < div className="about-card">
+          <img className="about-img" src={avatar_url} alt="avatar"/>
+        <div>
+        <h3>{name}</h3>
+        <h3>{location}</h3>
+        <h3>{bio}</h3>
+        <h3>{email}</h3>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default About
+export default About;
